ae5205a23c1b06f0c6fb17c55e0f8e67755c0efa
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2025-02-13 01:58:29 +0000

    navbar basic

---
+++ League/src/app/components/pages/ligues/ligues.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ligues',
+  standalone: true,
+  imports: [],
+  templateUrl: './ligues.component.html',
+  styleUrl: './ligues.component.css'
+})
+export class LiguesComponent {
+
+}
