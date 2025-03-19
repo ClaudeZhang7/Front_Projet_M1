3351b8dfdf7e09c2f3647c797787bcd45b55e218
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2025-03-19 16:48:37 +0000

    var

--- League/src/environments/environment.prod.ts
+++ League/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // correspond a l'url de notre api elle doit être caché !
     // l'url sera donné à travers les configs fait sur netlify.
-    apiUrl: 'https://back-projet-m1.onrender.com'
+    apiUrl: process.env['NG_APP_API_URL'] || ''
   };
