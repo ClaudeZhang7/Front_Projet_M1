ada0f0a57ead18c4ca55b2aae02fb5f0ee8b70ac
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2025-03-21 12:25:03 +0000

    test déploiement

--- League/src/environments/environment.prod.ts
+++ League/src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
     production: true,
     // correspond a l'url de notre api elle doit être caché !
     // l'url sera donné à travers les configs fait sur netlify.
-    apiUrl: process.env['NG_APP_API_URL'] || ''
+    apiUrl: typeof window !== 'undefined' && (window as any).NG_APP_API_URL 
+    ? (window as any).NG_APP_API_URL 
+    : ''
+
   };
