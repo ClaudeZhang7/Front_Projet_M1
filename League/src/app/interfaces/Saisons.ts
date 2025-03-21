3072e4208769f48e4904675b7880b47c19468a61
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2025-03-21 10:48:33 +0000

    mapping pour saisons (pas encore testé attente de la prod)

---
+++ League/src/app/interfaces/Saisons.ts
@@ -0,0 +1,10 @@
+export interface Saison {
+    id: number;
+    league_id: number;
+    debut: string;            
+    fin: string;              
+    nb_equipes: number;
+    nb_arbitres: number;
+    nb_remplacements: number;
+  }
+  
